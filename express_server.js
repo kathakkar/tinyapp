b8fc24d2f0f55fddf9b67f3b59ebd6406f58f411
Khushbu <khushbu.thakkar@outlook.com>
2021-08-31 22:05:41 +0000

    Add Edit Post operation

--- express_server.js
+++ express_server.js
@@ -13,18 +13,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//
+
+//Genrates random shortURL  
+const generateRandomString = function () {
+  let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 6; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 //Route to home PAGE
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
+//Show all urls GET
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+
+//ADD new url GET
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+    
+
+//Add new url POST
 app.post('/urls', (req, res) => {
   const shortUrl = generateRandomString().trim();
   urlDatabase[shortUrl] = req.body.longURL;
@@ -32,12 +51,8 @@ app.post('/urls', (req, res) => {
 
 });
 
-//Route to add new url
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-    
 
+//ADD new url GET operation to redirect Corresponding longURL
 app.get("/u/:shortURL", (req, res) => {
   if(urlDatabase[req.params.shortURL] != undefined){
     res.redirect(`${urlDatabase[req.params.shortURL]}`);
@@ -47,6 +62,7 @@ app.get("/u/:shortURL", (req, res) => {
   
 });
 
+//Delete url POST operation
 app.post("/urls/:shortURL/delete", (req, res) => {
 
   delete urlDatabase[req.params.shortURL];
@@ -55,21 +71,26 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+//Edit POST to load specific url
+app.post("/urls/:shortURL",(req,res) => {
+  const templateVars = {shortURL : req.params.shortURL, longURL : urlDatabase[req.params.shortURL]};
+  res.render("urls_show",templateVars);
 });
+
+//Edit GET operation to edit specific url
+app.post("/urls/:shortURL/edit",(req,res) => {
+  const shorturl = req.params.shortURL;
+  const longurl = req.body.longURL;
+  urlDatabase[shorturl] = longurl
+  res.redirect("/urls");
+});
+
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const generateRandomString = function () {
-
-  let result = ' ';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < 6; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-
-}
